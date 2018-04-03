0d5255d8dc97a8328719b7d935df70a3f1261cce
Edmagno <edmagnogomes@gmail.com>
2018-04-02 23:52:45 -0300

    Exercicio 1013 adicionado

---
+++ Iniciante/1013.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int a,b,c,maior1,maior2;
+scanf("%d %d %d",&a,&b,&c);
+maior1= (a+b+abs(a-b))/2;
+maior2= (c+maior1+abs(c-maior1))/2;
+printf("%d eh o maior\n",maior2);
+return 0;}
